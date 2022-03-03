01422af935f3dd109554f31af0a1b1f2841625a6
Leonid Gorkin <regnull@gmail.com>
2022-03-03 11:28:45 -0500

    russian and ukrainian translation

---
+++ html/ru/mail/common.js
@@ -0,0 +1,37 @@
+const TOKEN_NAME = "ubikom_token";
+const LOGIN_URL = "https://alpha.ubikom.cc:8899/login";
+const INBOX_URL = "https://alpha.ubikom.cc:8899/inbox";
+const MESSAGE_URL = "https://alpha.ubikom.cc:8899/message";
+const HTML_MESSAGE_URL = "https://alpha.ubikom.cc:8899/html-message";
+const SEND_URL = "https://alpha.ubikom.cc:8899/send";
+const DELETE_URL = "https://alpha.ubikom.cc:8899/delete";
+const MARK_AS_SEEN_URL = "https://alpha.ubikom.cc:8899/mark-as-seen"
+const GET_ATTACHMENT_URL = "https://alpha.ubikom.cc:8899/get-attachment"
+const UPLOAD_FILE_URL = "https://alpha.ubikom.cc:8899/upload-file"
+
+function formatAddressShort(f) {
+    if (f.Name != "") {
+        return f.Name;
+    }
+
+    return f.Address;
+}
+
+function formatAddress(f) {
+    if (f.Name == "") {
+        return f.Address;
+    }
+    return f.Name + " (" + f.Address + ")";
+}
+
+function formatAddressList(l) {
+    let r = "";
+    for (let i = 0; i < l.length; i++) {
+        let addr = l[i];
+        if (r != "") {
+            r += ", ";
+        }
+        r += formatAddress(addr)
+    }
+    return r
+}
