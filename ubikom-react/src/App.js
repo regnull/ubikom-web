efb7228bc1c9bbbf48730772d4e3191db0180a16
Leonid Gorkin <regnull@gmail.com>
2023-03-11 18:29:12 -0500

    wip

--- ubikom-react/src/App.js
+++ ubikom-react/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Container from "@mui/material/Container";
+import titleImage from "./static/images/title_image.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ubikom
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Box align="center" marginTop="50px">
+        <Typography variant="h2">Free and Secure Communications</Typography>
+      </Box>
+      <Box align="center" marginTop="50px">
+        <img src={titleImage} />
+      </Box>
+      <Box align="center" marginTop="50px">
+        <Typography variant="h4">Lorem Ipsum</Typography>
+        <Typography variant="body1">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Typography>
+      </Box>
+      <Stack spacing={2} direction="row">
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <Button variant="contained" marginLeft="20px">
+          Proceed
+        </Button>
+      </Stack>
+    </>
   );
 }
 
