36cf5f564de6663d06d66bf9cfe101321bb49598
Leonid Gorkin <regnull@gmail.com>
2023-03-12 15:24:11 -0400

    wip

--- ubikom-react/src/App.js
+++ ubikom-react/src/App.js
@@ -9,13 +9,18 @@ import MenuIcon from "@mui/icons-material/Menu";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider"
-import Container from "@mui/material/Container";
 import Avatar from '@mui/material/Avatar';
 import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import titleImage from "./static/images/title_image.jpg";
 import sonOfMan from "./static/images/son_of_man_round.png"
 
+import { useWeb3 } from '@openzeppelin/network/react';
+
+const infuraProjectId = '8f540714acb24862a8c9a5c3d8568f23';
+
 function App() {
+  const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
   return (
     <>
       <AppBar position="static">
@@ -32,7 +37,8 @@ function App() {
             <MenuIcon />
           </IconButton>
           <Box sx={{ flexGrow: 1 }} />
-          <EmailIcon />
+          <GitHubIcon onClick={() => window.open('https://github.com/regnull/ubikom')} sx={{ marginLeft: "10px", cursor: "pointer" }}/>
+          <EmailIcon sx={{ marginLeft: "10px" }}/>
           {/* <Button color="inherit">Login</Button> */}
         </Toolbar>
       </AppBar>
@@ -44,16 +50,25 @@ function App() {
         <Typography variant="subtitle1">Powered by Decentralized Private Identity</Typography>
       </Box>
       <Box align="center" marginTop="50px">
-        <Box maxWidth="800px">
-        <Typography variant="h4" marginBottom="20px">Lorem Ipsum</Typography>
-        <Typography variant="body1">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <Box maxWidth="800px" marginLeft="20px" marginRight="20px">
+        <Typography variant="h4" marginBottom="20px">The Identity You Own</Typography>
+        <Typography variant="body1" textAlign="left" >
+          Your account on Google, Meta, and other companies can be closed down at any time 
+          and for any reason. You don't own your identity with those companies (and even with
+          other privacy-focused companies). Unless you control your identity, it's not yours, and
+          you can only control it by controlling your encryption keys. With Ubikom, your identity
+          is your own. You create it on Ethereum blockchain, and you keep in self-custody at all
+          times.
+        </Typography>
+        </Box>
+      </Box>
+      <Box align="center" marginTop="50px">
+        <Box maxWidth="800px" marginLeft="20px" marginRight="20px">
+        <Typography variant="h4" marginBottom="20px">Communicate Privately</Typography>
+        <Typography variant="body1" textAlign="left" >
+          Using your identity, you can register a public key which may be used to send you private
+          messages. This key can be changed by any time - as long as you are in possession of your master
+          key.
         </Typography>
         </Box>
       </Box>
@@ -66,7 +81,7 @@ function App() {
         </Button>
       </Stack>
       </Box>
-      <Box height="50px" maxWidth="300px" alignContent="center" align="center">
+      <Box height="50px" maxWidth="300px" alignContent="center" align="center" sx={{ backgroundColor: "red"}}>
         <Divider/>
       </Box>
       <Typography variant="body1" marginLeft="100px">(c) Teralyt Software LLC, 2023</Typography>
